d07aae3f35e451da10d4e9bf73ed7e19130a8539
Gildas Metenou <gildas.metenou@abyster.com>
2023-12-03 19:30:21 +0100

    init projet kiri front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Message } from 'primeng/api';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MessageService] // or import globally in module Providers
 })
-export class AppComponent {
-  title = 'kiri_front';
+export class AppComponent implements OnInit {
+
+  constructor(private messageService: MessageService) { }
+
+  title = 'kiri front-end';
+
+  messages: Message[] = [];
+
+  ngOnInit() {
+    this.messages = [
+      { severity: 'success', summary: 'Success', detail: 'Bienvenu sur le projet kiri system front' }
+    ];
+  }
+
+
+  // show the toast on button click
+  showToast() {
+    this.messageService.add({ severity: 'success', summary: 'Heading', detail: 'More details....' });
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/**css msg */
+import { MessagesModule } from 'primeng/messages';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button'; // if toast showing on button click
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MessagesModule, //css template
+    ToastModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
