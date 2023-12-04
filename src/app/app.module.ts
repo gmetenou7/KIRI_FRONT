c08ea83e7aa299ae06e038c0d14170c544d2023c
Sonfack Nelson Mandela <patricknelson1995@gmail.com>
2023-12-04 05:28:26 +0100

    Added translation to the project, generate the bases module for the app (auth-module, dashboard module, shared-module, code-module), configured two environments for the project: environment.prod.ts and environment.ts).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 /**css msg */
 import { MessagesModule } from 'primeng/messages';
 import { ToastModule } from 'primeng/toast';
-import { ButtonModule } from 'primeng/button'; // if toast showing on button click
+import { ButtonModule } from 'primeng/button';
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core"; // if toast showing on button click
 
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
@@ -22,7 +27,15 @@ import { ButtonModule } from 'primeng/button'; // if toast showing on button cli
     BrowserAnimationsModule,
     MessagesModule, //css template
     ToastModule,
-    ButtonModule
+    ButtonModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
