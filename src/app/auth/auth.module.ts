079118190dfccc14ed07388f9ee91f2743a69d0b
Sonfack Nelson Mandela <patricknelson1995@gmail.com>
2023-12-29 18:41:27 +0100

    Fixed commit issues

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,36 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import { AuthRouting } from './auth-routing';
+import { AuRegisterComponent } from './au-register/au-register.component';
+import { AuLoginComponent } from './au-login/au-login.component';
+import {CheckboxModule} from "primeng/checkbox";
+import {PasswordModule} from "primeng/password";
+import {FormsModule} from "@angular/forms";
+import {ButtonModule} from "primeng/button";
+import {InputTextModule} from "primeng/inputtext";
+import {StyleClassModule} from "primeng/styleclass";
+import {CarouselModule} from "primeng/carousel";
+import {TagModule} from "primeng/tag";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuRegisterComponent,
+    AuLoginComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRouting,
+    CheckboxModule,
+    PasswordModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    StyleClassModule,
+    CarouselModule,
+    TagModule,
+    NgOptimizedImage
   ]
 })
 export class AuthModule { }
---
+++ src/app/auth/models/models.model.ts
@@ -0,0 +1,6 @@
+export type  Carousel = {
+  src: string;
+  alt: string;
+  header: string;
+  text: string;
+}
