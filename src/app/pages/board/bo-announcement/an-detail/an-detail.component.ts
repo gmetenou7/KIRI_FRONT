3883e99384c76845024ce0f9e0d75295c495f4ab
Sonfack Nelson Mandela <patricknelson1995@gmail.com>
2024-02-12 13:35:10 +0100

    Added announcements feature (list and add new ready, details and update added)

---
+++ src/app/pages/board/bo-announcement/an-detail/an-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-an-detail',
+  templateUrl: './an-detail.component.html',
+  styleUrls: ['./an-detail.component.css']
+})
+export class AnDetailComponent {
+
+}
---
+++ src/app/pages/board/bo-announcement/an-update/an-update.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-an-update',
+  templateUrl: './an-update.component.html',
+  styleUrls: ['./an-update.component.css']
+})
+export class AnUpdateComponent {
+
+}
