57a13e9534d28808e2150e49dc058a865c97bf23
Sonfack Nelson Mandela <patricknelson1995@gmail.com>
2024-02-12 01:26:08 +0100

    Adding announcements feature (list and add new ready)

--- src/app/pages/board/bo-announcement/bo-announcement.component.ts
+++ src/app/pages/board/bo-announcement/bo-announcement.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'app-bo-announcement',
   templateUrl: './bo-announcement.component.html',
   styleUrls: ['./bo-announcement.component.css']
 })
-export class BoAnnouncementComponent {
-
+export class BoAnnouncementComponent implements OnInit{
+  items: MenuItem[] = [];
+  showCreateModal: boolean = false
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Options',
+        items: [
+          {label: 'Voir', icon: 'pi pi-fw pi-eye'},
+          {label: 'Modfier', icon: 'pi pi-fw pi-file-edit'},
+          {label: 'Supprimer', icon: 'pi pi-fw pi-trash'}
+        ]
+      }];
+  }
 }
--- src/app/pages/board/board.module.ts
+++ src/app/pages/board/board.module.ts
@@ -6,18 +6,32 @@ import {ButtonModule} from "primeng/button";
 import {SharedModule} from "../../shared/shared.module";
 import { BoMainLayoutComponent } from './bo-main-layout/bo-main-layout.component';
 import { BoAnnouncementComponent } from './bo-announcement/bo-announcement.component';
+import {MenuModule} from "primeng/menu";
+import {CheckboxModule} from "primeng/checkbox";
+import {DialogModule} from "primeng/dialog";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import {InputTextModule} from "primeng/inputtext";
+import { AnCreateComponent } from './bo-announcement/an-create/an-create.component';
+import {FileUploadModule} from "primeng/fileupload";
 
 @NgModule({
   declarations: [
     BoCajComponent,
     BoMainLayoutComponent,
-    BoAnnouncementComponent
+    BoAnnouncementComponent,
+    AnCreateComponent
   ],
   imports: [
     CommonModule,
     BoardRouting,
     ButtonModule,
-    SharedModule
+    SharedModule,
+    MenuModule,
+    CheckboxModule,
+    DialogModule,
+    InputTextareaModule,
+    InputTextModule,
+    FileUploadModule
   ]
 })
 
