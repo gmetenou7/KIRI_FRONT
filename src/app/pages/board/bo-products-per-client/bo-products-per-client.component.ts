475f01913a364e7b0058ef4258f466b6f5d2149b
devItDookeCorporation <dev.it@dookecorporation.com>
2024-02-19 09:17:35 +0100

    type content correction on HTML template bo-products-per-client.component.html by replacing selectClient?.name by selectClient.name

--- src/app/pages/board/bo-products-per-client/bo-products-per-client.component.ts
+++ src/app/pages/board/bo-products-per-client/bo-products-per-client.component.ts
@@ -1,69 +1,68 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ProductModele} from "../../../core/models/product.modele";
-import {ClientModele} from "../../../core/models/client.modele";
-import {ClientService} from "../../../core/services/client.service";
-import {ProductService} from "../../../core/services/product.service";
-import {Table} from "primeng/table";
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ProductModele } from '../../../core/models/product.modele';
+import { ClientModele } from '../../../core/models/client.modele';
+import { ClientService } from '../../../core/services/client.service';
+import { ProductService } from '../../../core/services/product.service';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-bo-products-per-client',
   templateUrl: './bo-products-per-client.component.html',
-  styleUrls: ['./bo-products-per-client.component.css']
+  styleUrls: ['./bo-products-per-client.component.css'],
 })
 export class BoProductsPerClientComponent implements OnInit {
   years: Array<number> = [];
-  agencies:Array<{name: string, id:number, ca: number}> = [];
+  agencies: Array<{ name: string; id: number; ca: number }> = [];
   products: ProductModele[] = [];
   clients: ClientModele[] = [];
-  loading:boolean = false;
+  loading: boolean = false;
   showSendMessageDrawer: boolean = false;
 
   selectedYear: number = 2024;
-  selectedAgency: {name:string, id: number, ca:number} = {
+  selectedAgency: { name: string; id: number; ca: number } = {
     id: 1,
-    name: "Agence de Yaoundé",
-    ca: 100000000
+    name: 'Agence de Yaoundé',
+    ca: 100000000,
   };
-  selectClient: ClientModele | undefined;
-  selectedClients: ClientModele[] = []
+  selectClient!: ClientModele | undefined;
+  selectedClients: ClientModele[] = [];
 
   @ViewChild('filter') filter!: ElementRef;
   constructor(
     private clientService: ClientService,
     private productService: ProductService
-  ) {
-  }
+  ) {}
 
   ngOnInit() {
     this.years = this.getYearsUntil(2000);
     this.clientService.getAllClient().then((data) => {
       this.loading = false;
-      this.clients = data
+      this.clients = data;
     });
-    this.productService.getAllProduct().then(products => {
+    this.productService.getAllProduct().then((products) => {
       this.loading = false;
-      this.products = products
+      this.products = products;
     });
     this.agencies = [
       {
         id: 1,
-        name: "Agence de Yaoundé",
-        ca: 100000000
+        name: 'Agence de Yaoundé',
+        ca: 100000000,
       },
       {
         id: 2,
-        name: "Agence de Douala",
-        ca: 200000000
+        name: 'Agence de Douala',
+        ca: 200000000,
       },
       {
         id: 3,
-        name: "Agence de Dshang",
-        ca: 300000000
-      }
+        name: 'Agence de Dshang',
+        ca: 300000000,
+      },
     ];
   }
 
-  getYearsUntil(year:number) {
+  getYearsUntil(year: number) {
     let currentYear = new Date().getFullYear();
     let yearsArray = [];
     for (let i = currentYear; i >= year; i--) {
@@ -79,11 +78,11 @@ export class BoProductsPerClientComponent implements OnInit {
   load() {
     this.loading = true;
     setTimeout(() => {
-      this.loading = false
+      this.loading = false;
     }, 2000);
   }
 
   onSendMessageDrawer() {
-    this.showSendMessageDrawer = !this.showSendMessageDrawer
+    this.showSendMessageDrawer = !this.showSendMessageDrawer;
   }
 }
