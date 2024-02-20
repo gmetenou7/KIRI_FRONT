9c7447e6978d6dbb89283fcc79c29c1ae461bb0f
Sonfack Nelson Mandela <patricknelson1995@gmail.com>
2024-02-20 10:41:53 +0100

    Added active route indicator in the left pages side bare

--- src/app/pages/board/bo-main-layout/bo-main-layout.component.ts
+++ src/app/pages/board/bo-main-layout/bo-main-layout.component.ts
@@ -1,13 +1,13 @@
-import {Component, OnInit} from '@angular/core';
-import {PageSidebarLinkModele} from "../../../core/models/page-sidebar-link.modele";
-import {DashboardPageSideItems} from "../../../core/data/page-side-items.data";
+import { Component, OnInit } from '@angular/core';
+import { PageSidebarLinkModele } from "../../../core/models/page-sidebar-link.modele";
+import { DashboardPageSideItems } from "../../../core/data/page-side-items.data";
 
 @Component({
   selector: 'app-bo-main-layout',
   templateUrl: './bo-main-layout.component.html',
   styleUrls: ['./bo-main-layout.component.css']
 })
-export class BoMainLayoutComponent implements OnInit{
+export class BoMainLayoutComponent implements OnInit {
   leftSidebarItems: PageSidebarLinkModele[] = []
 
   ngOnInit() {
