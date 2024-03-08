0fdfe54cda22ed6cfe8d713c866aca0621b09aaa
devItDookeCorporation <dev.it@dookecorporation.com>
2024-03-08 09:22:31 +0100

    correction du conflit de la tentative de merge sur la dev

--- src/app/shared/sh-notfount/sh-notfount.component.ts
+++ src/app/shared/sh-notfount/sh-notfount.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-sh-notfount',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./sh-notfount.component.css']
 })
 export class ShNotFountComponent {
+
+  private history: string[] = [];
+
+  constructor(
+    private router: Router,
+    private location: Location
+  ) {
+  }
+
+  onRouterBack() {
+    this.location.back()
+  }
 
 }
